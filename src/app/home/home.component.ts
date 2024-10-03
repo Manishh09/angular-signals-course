0502d66fece7bfaaa413245cf515365359c64a1d
Manish Boge (SX/EDC2) <manish.boge@in.bosch.com>
2024-10-03 21:56:54 +0530

    refactor: Remove unused code in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,45 +21,4 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
 })
 export class HomeComponent {
 
-  count = signal(0) // By Default , here the signal variable is of of WritableSignal type.
-
-  readlySignalVar = signal(0).asReadonly(); // Here, the signal is ReadOnly
-
-  signalWithObj = signal<Counter>({
-    value: 10
-  })
-
-  inc(){
-    this.count.update(c => c + 1)
-  }
-
-  dec() {
-    this.count.update(c => c - 1)
-  }
-
-  set() {
-    this.count.set( this.count() + 1)
-  }
-
-  updateSignalsWithObj() {
-    //this.signalWithObj().value ++ // should not do this. we loose the signal features
-    // signalWithObj is Writable Signal of type Counter
-    this.signalWithObj.update( (v: Counter) => ({
-      ...v,
-      value:  v.value + 1
-    })
-    )
-  }
-
-
-  modifySignalVal() {
-    // modification is not allowed on read-only signals
-    // this.readlySignalVar.set(this.readlySignalVar() + 1);
-    // this.readlySignalVar.update(c =>  c + 1); 
-  }
-
-}
-
-type Counter = {
-  value : number
 }
